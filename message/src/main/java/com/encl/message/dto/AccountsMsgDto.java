021c1f0139634ad3f84acb1c55af1a7a58c6a626
mjlslink <mjlslink5@gmail.com>
2025-10-24 14:06:18 -0500

    initial

---
+++ message/src/main/java/com/encl/message/dto/AccountsMsgDto.java
@@ -0,0 +1,10 @@
+package com.encl.message.dto;
+
+/**
+ * @param accountNumber
+ * @param name
+ * @param email
+ * @param mobileNumber
+ */
+public record AccountsMsgDto(Long accountNumber, String name, String email, String mobileNumber) {
+}
