f91ff56624d83ee7779953e4c14dd14939f8d02a
mjlslink <mjlslink5@gmail.com>
2025-10-24 15:22:50 -0500

    s17:service discovery adapted

--- gatewayserver/src/main/java/com/encl/gatewayserver/GatewayserverApplication.java
+++ gatewayserver/src/main/java/com/encl/gatewayserver/GatewayserverApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.circuitbreaker.resilience4j.ReactiveResilience4JCircuitBreakerFactory;
 import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
 import org.springframework.cloud.client.circuitbreaker.Customizer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
 import org.springframework.cloud.gateway.filter.ratelimit.RedisRateLimiter;
 import org.springframework.cloud.gateway.route.RouteLocator;
@@ -19,6 +20,7 @@ import java.time.Duration;
 import java.time.LocalDateTime;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class GatewayserverApplication {
 
 	public static void main(String[] args) {
@@ -34,7 +36,8 @@ public class GatewayserverApplication {
 										.addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
 										.circuitBreaker(config -> config.setName("accountsCircuitBreaker")
 												.setFallbackUri("forward:/contactSupport")))
-								.uri("lb://ACCOUNTS"))
+					//			.uri("lb://ACCOUNTS"))
+								.uri("http://accounts:8080"))
 					.route(p -> p
 							.path("/bankapp/loans/**")
 							.filters( f -> f.rewritePath("/bankapp/loans/(?<segment>.*)","/${segment}")
@@ -42,15 +45,19 @@ public class GatewayserverApplication {
 									.retry(retryConfig -> retryConfig.setRetries(3)
 											.setMethods(HttpMethod.GET)
 											.setBackoff(Duration.ofMillis(100),Duration.ofMillis(1000),2,true)))
-							.uri("lb://LOANS"))
-					.route(p -> p
+//							.uri("lb://LOANS"))
+                            .uri("http://loans:8090"))
+
+                .route(p -> p
 							.path("/bankapp/cards/**")
 							.filters( f -> f.rewritePath("/bankapp/cards/(?<segment>.*)","/${segment}")
 									.addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
 									.requestRateLimiter(config -> config.setRateLimiter(redisRateLimiter())
 											.setKeyResolver(userKeyResolver())))
-							.uri("lb://CARDS")).build();
-	}
+							//.uri("lb://CARDS")).build();
+                        .uri("http://loans:9000")).build();
+
+    }
 
 	@Bean
 	public Customizer<ReactiveResilience4JCircuitBreakerFactory> defaultCustomizer() {
